751049b8042860e46851e8de33ec79f6a189f642
vicky <vickykumarsunilja@gmail.com>
2024-02-06 15:50:07 +0530

    added vercel.json config file

--- backend/src/helpers/s3.util.ts
+++ backend/src/helpers/s3.util.ts
@@ -1,10 +1,10 @@
 import {S3Client} from "@aws-sdk/client-s3";
 
 const config = {
-  region: process.env.AWS_REGION!,
+  region: process.env.AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_KEY!,
+    accessKeyId: process.env.AWS_TOKEN_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_TOKEN_SECRET_KEY!,
   },
 };
 export const s3 = new S3Client(config);
